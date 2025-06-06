3f739a2042b664aca1502acf1b0a456922e545a5
ryramzy <mramsay0@gmail.com>
2025-06-06 13:25:06 -0300

    Update logo image source to use new logo file

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -31,7 +31,7 @@ export function Header() {
           {/* Logo */}
           <Link href="/" className="flex items-center">
             <Image
-              src="/logo.png"
+              src="/logo"
               alt="Garry Payton Law"
               width={40}
               height={40}
